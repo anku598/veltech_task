e71f10ae6e3c2e5b28de79ec8e9b36b902ea6baf
anik <anik598@gmail.com>
2022-07-06 14:15:14 +0600

    build error fix

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   assetsDir: "static",
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-
-        if (process.env.NODE_ENV === 'production') {
-          args[0].inject = "body";
-          args[0].template = 'src/templates/index.html';
-        }
-        return args;
-      })
-
-  },
   css: {
     loaderOptions: {
       sass: {
